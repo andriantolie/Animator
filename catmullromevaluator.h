d863c25c40a4b45943a40cba4d968e0c2bc3c667
shiftahead <shiftahead@gmail.com>
2014-11-25 10:29:26 +0800

    Catmull Rom curve almost done
    Small bugs exist, needs more work later

---
+++ catmullromevaluator.h
@@ -0,0 +1,19 @@
+#ifndef INCLUDED_CATMULLROM_CURVE_EVALUATOR_H
+#define INCLUDED_CATMULLROM_CURVE_EVALUATOR_H
+
+#pragma warning(disable : 4786)  
+
+#include "CurveEvaluator.h"
+
+class CatmullRomEvaluator : public CurveEvaluator
+{
+public:
+	void evaluateCurve(const std::vector<Point>& ptvCtrlPts,
+		std::vector<Point>& ptvEvaluatedCurvePts,
+		const float& fAniLength,
+		const bool& bWrap) const;
+	void displayCatmullRom(Point v0, Point v1, Point v2, Point v3, std::vector<Point>& ptvEvaluatedCurvePts, float fAniLength) const;
+	std::vector<Point> convertToBezier(Point p0, Point p1, Point p2, Point p3) const;
+};
+
+#endif
--- graphwidget.cpp
+++ graphwidget.cpp
@@ -27,6 +27,7 @@
 #include "LinearCurveEvaluator.h"
 #include "BezierCurveEvaluator.h"
 #include "BSplineEvaluator.h"
+#include "CatmullRomEvaluator.h"
  
 
 #define LEFT		1
@@ -120,7 +121,7 @@ m_flcCurrCurve(FL_BLACK)
 	// TODO: replace the linear evaluator for one of the three types of curves
 	m_ppceCurveEvaluators[CURVE_TYPE_BSPLINE] = new BSplineEvaluator();
 	m_ppceCurveEvaluators[CURVE_TYPE_BEZIER] = new BezierCurveEvaluator();
-	m_ppceCurveEvaluators[CURVE_TYPE_CATMULLROM] = new LinearCurveEvaluator();
+	m_ppceCurveEvaluators[CURVE_TYPE_CATMULLROM] = new CatmullRomEvaluator();
 	// Note that C2-Interpolating curve is not a requirement
 	m_ppceCurveEvaluators[CURVE_TYPE_C2INTERPOLATING] = new LinearCurveEvaluator();
 
