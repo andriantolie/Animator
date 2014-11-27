87a204782eac78f7f91e1635555d42f1606808b4
Andrianto <andrianto.lie@gmail.com>
2014-11-27 13:30:53 +0800

    Fix wrapping for c2interpolation

--- c2interpolationevaluator.cpp
+++ c2interpolationevaluator.cpp
@@ -12,52 +12,26 @@ void C2InterpolationEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPt
 	ptvEvaluatedCurvePts.clear();
 	int iCtrlPtCount = ctrlPts.size();
 
+	// find velocity vectors
+	std::vector<Point> velocityVectors = calculateVelocity(ctrlPts, bWrap, fAniLength);
+
 	if (!bWrap){
 		// if the curve is not wrapped, make the beginning and the end of the curve horizontal
 		ptvEvaluatedCurvePts.push_back(Point(0.0, ctrlPts[0].y));
 		ptvEvaluatedCurvePts.push_back(Point(fAniLength, ctrlPts[iCtrlPtCount - 1].y));
 
+		ptvEvaluatedCurvePts.push_back(ctrlPts[0]);
+		ptvEvaluatedCurvePts.push_back(ctrlPts[iCtrlPtCount - 1]);
 	}
 	else{
-	/*	double shadowPointHeadX = ctrlPts[iCtrlPtCount - 1].x - fAniLength;
-		double shadowPointHeadY = ctrlPts[iCtrlPtCount - 1].y;
-		Point shadowPointHead(shadowPointHeadX, shadowPointHeadY);*/
-		//double shadowPointHead1X = ctrlPts[iCtrlPtCount - 2].x - fAniLength;
-		//double shadowPointHead1Y = ctrlPts[iCtrlPtCount - 2].y;
-		//Point shadowPointHead1(shadowPointHead1X, shadowPointHead1Y);
-
-			// create shadow control points for wrapping
-			double shadowPointTail0X = fAniLength + ctrlPts[0].x;
-			double shadowPointTail0Y = ctrlPts[0].y;
-			Point shadowPointTail0(shadowPointTail0X, shadowPointTail0Y);
-
-
-		//	double shadowPointTail1X = fAniLength + ctrlPts[1].x;
-		//	double shadowPointTail1Y = ctrlPts[1].y;
-		//	Point shadowPointTail1(shadowPointTail1X, shadowPointTail1Y);
-
-		//	//double shadowPointTail2X = fAniLength + ctrlPts[2].x;
-		//	//double shadowPointTail2Y = ctrlPts[2].y;
-		//	//Point shadowPointTail2(shadowPointTail2X, shadowPointTail2Y);
-
-		//	ctrlPts.insert(ctrlPts.begin(), shadowPointHead1);
-			//ctrlPts.insert(ctrlPts.begin(), shadowPointHead);
-			ctrlPts.push_back(shadowPointTail0);
-		//	ctrlPts.push_back(shadowPointTail1);
-		//	//ctrlPts.push_back(shadowPointTail2);
-			iCtrlPtCount = ctrlPts.size();
-
-		
+		displayC2(Point(ctrlPts[iCtrlPtCount - 1].x - fAniLength, ctrlPts[iCtrlPtCount - 1].y), ctrlPts[0], velocityVectors[iCtrlPtCount - 1], velocityVectors[0], ptvEvaluatedCurvePts, fAniLength);
+		displayC2(ctrlPts[iCtrlPtCount - 1], Point(ctrlPts[0].x + fAniLength, ctrlPts[0].y), velocityVectors[iCtrlPtCount - 1], velocityVectors[0], ptvEvaluatedCurvePts, fAniLength);
 
 	}
 	
-	// find velocity vectors
-	std::vector<Point> velocityVectors = calculateVelocity(ctrlPts, bWrap, fAniLength);
-
 	for (int i = 0; i + 1 < iCtrlPtCount; i++){
-		displayC2(ctrlPts[i], ctrlPts[i + 1], velocityVectors[i], velocityVectors[i+1], ptvEvaluatedCurvePts, fAniLength);
+			displayC2(ctrlPts[i], ctrlPts[i + 1], velocityVectors[i], velocityVectors[i + 1], ptvEvaluatedCurvePts, fAniLength);
 	}
-
 
 }
 
@@ -75,9 +49,10 @@ void C2InterpolationEvaluator::displayC2(Point c0, Point c1, Point d0, Point d1,
 
 		double resultX = (1 - u)*v0DoublePrime.x + u*v1DoublePrime.x;
 		double resultY = (1 - u)*v0DoublePrime.y + u*v1DoublePrime.y;
-		if (resultX >= fAniLength) resultX = resultX - fAniLength;
+		if (resultX <= fAniLength && resultX >= 0){
 			Point result(resultX, resultY);
 			ptvEvaluatedCurvePts.push_back(result);
+		}
 	}
 
 }
@@ -101,6 +76,7 @@ std::vector<Point> C2InterpolationEvaluator::calculateVelocity(std::vector<Point
 			matrix[ctrlPtCnt - 1][0] = 1;
 		}
 		else{
+
 			matrix[0][0] = 2;
 			matrix[0][1] = 1;
 			matrix[ctrlPtCnt - 1][ctrlPtCnt - 2] = 1;
@@ -138,28 +114,41 @@ std::vector<Point> C2InterpolationEvaluator::calculateVelocity(std::vector<Point
 
 		// build upper triangle matrix
 		for (int i = 0; i < ctrlPtCnt; i++){
-			double multiplyingFactor = 1 / matrix[i][i];
+			double multiplyingFactor = 1.0 / matrix[i][i];
 			for (int j = 0; j < ctrlPtCnt; j++){
 				matrix[i][j] *= multiplyingFactor;
-				if(i+ 1 < ctrlPtCnt) matrix[i + 1][j] -= matrix[i][j];
+				for (int k = i + 1; k < ctrlPtCnt; k++){
+					matrix[k][j] -= matrix[i][j] * matrix[k][i];
+				}
+
 			}
+
 			rightPartOfMatrix[i].x *= multiplyingFactor;
 			rightPartOfMatrix[i].y *= multiplyingFactor;
 
-			if (i + 1 < ctrlPtCnt){
-				rightPartOfMatrix[i + 1].x -= rightPartOfMatrix[i].x;
-				rightPartOfMatrix[i + 1].y -= rightPartOfMatrix[i].y;
+			for (int k = i + 1; k < ctrlPtCnt; k++){
+
+				rightPartOfMatrix[k].x -= rightPartOfMatrix[i].x * matrix[k][i];
+				rightPartOfMatrix[k].y -= rightPartOfMatrix[i].y * matrix[k][i];
 			}
+
 		}
 		
 		// build diagonal matrix
 		for (int i = ctrlPtCnt - 1; i > 0; i--){
 			double multiplyingFactor = matrix[i-1][i];
 			for (int j = 0; j < ctrlPtCnt; j++){
-				matrix[i - 1][j] -= matrix[i][j] * multiplyingFactor;
+
+				for (int k = i - 1; k >= 0; k--){
+					matrix[k][j] -= matrix[i][j] * matrix[k][i];
+				}
 			}
-			rightPartOfMatrix[i - 1].x -= rightPartOfMatrix[i].x * multiplyingFactor;
-			rightPartOfMatrix[i - 1].y -= rightPartOfMatrix[i].y * multiplyingFactor;
+
+			for (int k = i - 1; k >= 0; k--){
+				rightPartOfMatrix[k].x -= rightPartOfMatrix[i].x * matrix[k][i];
+				rightPartOfMatrix[k].y -= rightPartOfMatrix[i].y * matrix[k][i];
+			}
+
 		}
 
 		// delete the matrix
